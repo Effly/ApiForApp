ac599c24d770d70a988e1920182dc9c33f63a7d9
Effly <effly88@gmail.com>
2021-07-21 13:33:45 +0300

    changePassword -> для роута  смена пароля

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use App\Models\Codes;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
         $validatedData = $request->validate([
-            'name'=>'string',
+            'name' => 'string',
             'email' => 'email|required|unique:users',
             'password' => 'required',
-            'tel'=>'string'
+            'tel' => 'string'
         ]);
 
         $validatedData['password'] = bcrypt($request->password);
@@ -25,7 +29,7 @@ class AuthController extends Controller
 
         $accessToken = $user->createToken('authToken')->accessToken;
 
-        return response([ 'user' => $user, 'access_token' => $accessToken]);
+        return response(['user' => $user, 'access_token' => $accessToken]);
     }
 
     public function login(Request $request)
@@ -46,7 +50,54 @@ class AuthController extends Controller
 //        return response(['user' => auth()->user()]);
 
     }
-    public function getCode(Codes $codes){
-        return response(['code'=>$codes->getCode()]);
+
+    public function getCode(Codes $codes)
+    {
+        return response(['code' => $codes->getCode()]);
+    }
+
+    public function changePassword(Request $request)
+    {
+
+        $rules = array(
+            'email' => 'email|required',
+            'new_password' => 'required|min:8',
+            'confirm_password' => 'required|same:new_password',
+            'code' => 'required|min:5'
+        );
+
+        $messages = array(
+            'required' => 'Обязательно должен быть заполнен :attribute',
+            'email' => 'The :attribute field is email',
+            'same:new_password' => 'The :attribute field must only be letters and numbers (no spaces)',
+            'min:8' => 'The :attribute field must only be letters and numbers (8)',
+            'min:5' => 'The :attribute field must only be letters and numbers (5)'
+        );
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $user = User::where('email', $request->email)->first();
+//        dd($user->last_code);
+        if ((string)$user->last_code == $request->code) {
+            if (Hash::check($request->new_password, $user->password) == false) {
+//                dd($user->password);
+                $user->password = Hash::make($request->new_password);
+//                $user->password = $request->new_password;
+                $user->save();
+                $response['status'] = 'OK';
+                $response['desc'] = 'Password reset successfully';
+            } else {
+                $response['status']='ERROR';
+                $response['desc'] = 'It\'s old password';
+            }
+        } else{
+            $response['status']='ERROR';
+            $response['desc'] = 'INVALID_CODE';
+        }
+
+        return response()->json($response);
     }
 }
