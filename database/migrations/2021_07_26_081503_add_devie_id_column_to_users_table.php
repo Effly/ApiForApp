5d29653d5909ca035b39a329e719009e6e4b4962
Effly <effly88@gmail.com>
2021-07-26 17:57:45 +0300

    Добавить столбец device_id

---
+++ database/migrations/2021_07_26_081503_add_devie_id_column_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDevieIdColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('device_id')->after('last_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('device_id');
+        });
+    }
+}
