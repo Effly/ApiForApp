d27baabf401be544a796b404dd191089c9769d1c
Effly <effly88@gmail.com>
2021-07-27 09:16:52 +0300

    Добавить сохранение полисов в базу данных и счетчик полисов

--- app/Models/Policies.php
+++ app/Models/Policies.php
@@ -11,6 +11,44 @@ class Policies extends Model
 
     public function fetchPolicies($user_id)
     {
-        return $this->where('user_id',$user_id)->get();
+        return $this->where('user_id', $user_id)->get();
+    }
+
+    public function savePolicy($data,$user_id)
+    {
+        $data['number'] = $this->getCounter();
+        $policy = new Policies();
+        $policy->policy_data = serialize($data);
+        $policy->user_id = $user_id;
+        $save = $policy->save();
+        if ($save)  return [
+
+            'policy_number' => $data['number'],
+            'policy_series' => 'ser.',
+            'ins_sum' => ins_sum,
+            'ins_premium' => ins_premium,
+            'rules_link' => 'url',
+        ];
+        else return false;
+    }
+    public function getCounter()
+    {
+        $file_counter = 'counter.dat';
+
+        // Читаем текущее значение счетчика
+        if (file_exists($file_counter)) {
+            $fp = fopen($file_counter, "r");
+            $counter = fread($fp, filesize($file_counter));
+            fclose($fp);
+        } else {
+            $counter = 000000000001;
+        }
+        // Увеличиваем счетчик на единицу
+        $counter++;
+        // Сохраняем обновленное значение счетчика
+        $fp = fopen($file_counter, "w");
+        fwrite($fp, $counter);
+        fclose($fp);
+        return $counter;
     }
 }
